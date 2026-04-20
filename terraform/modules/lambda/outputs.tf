7f628f535b7b4d31d5d3c0f72e136160ae784b56
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-20 23:02:50 +0530

    Update outputs.tf

--- terraform/modules/lambda/outputs.tf
+++ terraform/modules/lambda/outputs.tf
@@ -45,3 +45,59 @@ output "add_candidates_function_arn" {
 output "add_candidates_function_name" {
   value = aws_lambda_function.add_candidates.function_name
 }
+
+output "get_posts_function_arn" {
+  value = aws_lambda_function.get_posts.arn
+}
+
+output "get_posts_function_name" {
+  value = aws_lambda_function.get_posts.function_name
+}
+
+output "export_results_function_arn" {
+  value = aws_lambda_function.export_results.arn
+}
+
+output "export_results_function_name" {
+  value = aws_lambda_function.export_results.function_name
+}
+
+output "bulk_upload_voters_function_arn" {
+  value = aws_lambda_function.bulk_upload_voters.arn
+}
+
+output "bulk_upload_voters_function_name" {
+  value = aws_lambda_function.bulk_upload_voters.function_name
+}
+
+output "admin_login_function_arn" {
+  value = aws_lambda_function.admin_login.arn
+}
+
+output "admin_login_function_name" {
+  value = aws_lambda_function.admin_login.function_name
+}
+
+output "admin_stats_function_arn" {
+  value = aws_lambda_function.admin_stats.arn
+}
+
+output "admin_stats_function_name" {
+  value = aws_lambda_function.admin_stats.function_name
+}
+
+output "get_elections_function_arn" {
+  value = aws_lambda_function.get_elections.arn
+}
+
+output "get_elections_function_name" {
+  value = aws_lambda_function.get_elections.function_name
+}
+
+output "get_candidates_function_arn" {
+  value = aws_lambda_function.get_candidates.arn
+}
+
+output "get_candidates_function_name" {
+  value = aws_lambda_function.get_candidates.function_name
+}
