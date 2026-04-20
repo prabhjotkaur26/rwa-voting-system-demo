d06318307bebf99f68a0a72a9de7d2864efac719
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-20 22:48:52 +0530

    Refactor variables in variables.tf for conciseness

--- terraform/modules/lambda/variables.tf
+++ terraform/modules/lambda/variables.tf
@@ -1,51 +1,16 @@
-variable "project_name" {
-  type = string
-}
+variable "project_name" { type = string }
+variable "environment" { type = string }
+variable "lambda_timeout" { type = number }
+variable "lambda_memory" { type = number }
+variable "execution_role_arn" { type = string }
 
-variable "environment" {
-  type = string
-}
+variable "votes_table_name" { type = string }
+variable "otp_table_name" { type = string }
+variable "candidates_table_name" { type = string }
+variable "elections_table_name" { type = string }
+variable "voters_table_name" { type = string }
 
-variable "lambda_timeout" {
-  type = number
-}
+variable "sns_topic_arn" { type = string }
 
-variable "lambda_memory" {
-  type = number
-}
-
-variable "execution_role_arn" {
-  type = string
-}
-
-variable "votes_table_name" {
-  type = string
-}
-
-variable "otp_table_name" {
-  type = string
-}
-
-variable "candidates_table_name" {
-  type = string
-}
-
-variable "elections_table_name" {
-  type = string
-}
-
-variable "voters_table_name" {
-  type = string
-}
-
-variable "sns_topic_arn" {
-  type = string
-}
-
-variable "otp_expiry_minutes" {
-  type = number
-}
-
-variable "aws_region" {
-  type = string
-}
+variable "otp_expiry_minutes" { type = number }
+variable "aws_region" { type = string }
