dece714cd3dc876b4a638dafcb429435e6cfdf48
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-20 22:56:02 +0530

    Add outputs for multiple Lambda functions

--- terraform/modules/lambda/outputs.tf
+++ terraform/modules/lambda/outputs.tf
@@ -1,16 +1,47 @@
-variable "project_name" { type = string }
-variable "environment" { type = string }
-variable "lambda_timeout" { type = number }
-variable "lambda_memory" { type = number }
-variable "execution_role_arn" { type = string }
+output "send_otp_function_arn" {
+  value = aws_lambda_function.send_otp.arn
+}
 
-variable "votes_table_name" { type = string }
-variable "otp_table_name" { type = string }
-variable "candidates_table_name" { type = string }
-variable "elections_table_name" { type = string }
-variable "voters_table_name" { type = string }
+output "send_otp_function_name" {
+  value = aws_lambda_function.send_otp.function_name
+}
 
-variable "sns_topic_arn" { type = string }
+output "verify_otp_function_arn" {
+  value = aws_lambda_function.verify_otp.arn
+}
 
-variable "otp_expiry_minutes" { type = number }
-variable "aws_region" { type = string }
+output "verify_otp_function_name" {
+  value = aws_lambda_function.verify_otp.function_name
+}
+
+output "cast_vote_function_arn" {
+  value = aws_lambda_function.cast_vote.arn
+}
+
+output "cast_vote_function_name" {
+  value = aws_lambda_function.cast_vote.function_name
+}
+
+output "get_results_function_arn" {
+  value = aws_lambda_function.get_results.arn
+}
+
+output "get_results_function_name" {
+  value = aws_lambda_function.get_results.function_name
+}
+
+output "create_election_function_arn" {
+  value = aws_lambda_function.create_election.arn
+}
+
+output "create_election_function_name" {
+  value = aws_lambda_function.create_election.function_name
+}
+
+output "add_candidates_function_arn" {
+  value = aws_lambda_function.add_candidates.arn
+}
+
+output "add_candidates_function_name" {
+  value = aws_lambda_function.add_candidates.function_name
+}
